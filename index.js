7b877f8fb06f888c8b34f4a02769191c495cd0eb
Mateus Cavalcante <mateuscsantos2501@gmail.com>
2024-05-14 23:50:47 -0300

    correção caminho

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import express from 'express'
-
 import path from 'path';
 
 const porta = 3000;
@@ -7,8 +6,9 @@ const host = '0.0.0.0';
 
 var listarPacientes = [];
 
-
 const app = express();
+
+app.use(express.static(path.join(process.cwd(), 'publico')));
     
 app.use('/cadastroPacientes', (req,resp)=>{
 
@@ -77,10 +77,6 @@ app.use('/listarPacientes', (req,resp)=>{
     resp.write('</html>');
     resp.end();
 });
-
-
-
-app.use(express.static('./publico'))
 
 app.listen(porta, host, () => {
     console.log(`Servidor executando na porta http://${host}:${porta}`);
